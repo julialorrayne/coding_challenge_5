027d95842834bb3e6e3659200b30544804a85aa9
Julia <lorrayne@usf.edu>
2025-02-09 16:11:57 -0500

    Task 2 - Order Details.

--- cc_5.js
+++ cc_5.js
@@ -7,3 +7,16 @@ let customer = {
 
 console.log(`Customer info: name: ${customer.name}, age: ${customer.age}, email: ${customer.email}`); 
 //Logged each property 
+
+//Task 2: Object Methods
+let order = {
+    orderId: 1234,
+    totalAmount: 150,
+    status: "Processing",
+    displayOrder: function() {
+        return console.log(`Order ID: ${this.orderId}, Total Amount: ${this.totalAmount}, Status: ${this.status}`)
+    }
+}; //created object purchase
+
+order.displayOrder(); //called method
+
